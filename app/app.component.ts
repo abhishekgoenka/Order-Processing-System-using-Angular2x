233d5c085352beee79b213905423454bf659a336
Goenka <Abhishek.Goenka@allscripts.com>
2017-01-28 12:29:49 +0530

    Added app.component

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+//
+
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ops-app',
+    template: '<h1> Angular getting started </h1>'
+})
+
+export class AppComponent {
+    pageTitle: String = 'Order Processing System';
+}
+
