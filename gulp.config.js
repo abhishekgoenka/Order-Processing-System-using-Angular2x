9b565ca3bf7c0eaac2d9e369239cfad93fbff17c
Goenka <Abhishek.Goenka@allscripts.com>
2017-02-15 05:10:56 +0530

    Fixed gulp copy

--- gulp.config.js
+++ gulp.config.js
@@ -1,7 +1,11 @@
 module.exports = function () {
     'use strict';
     var config = {
-        jsfiles : ['./app/*.js'],
+        jsfiles: ['./app/*.js', './app/**/*.js', './bower_components/bootstrap/dist/css/bootstrap.css'],
+        
+        venderjsfiles: ['./**/jquery/dist/jquery.js', './**/jquery-validation/dist/jquery.validate.js', './**/core-js/client/shim.min.js'],
+        cssfiles: ['./app/**/*.css', './bower_components/bootstrap/dist/css/bootstrap.css'],
+        vendercssfiles: ['./**/bootstrap/dist/**/bootstrap.css', './**/font-awesome/css/font-awesome.css', '!node_modules/**'],
         build: './build/'
     };
     return config;
--- gulpfile.js
+++ gulpfile.js
@@ -11,11 +11,11 @@ var $ = require('gulp-load-plugins')({
 gulp.task('help', $.taskListing);
 // This will run in this order:
 // * build-clean
-// * build-scripts and build-styles in parallel
+// * build-scripts, build-styles, build-external-styles and build-external-scripts in parallel
 // * build-html
 // * Finally call the callback function
 gulp.task('build', function (callback) {
-    runSequence('build-clean', ['build-scripts'],
+    runSequence('build-clean', ['build-scripts', 'build-styles', 'build-external-styles', 'build-external-scripts'],
         callback);
 });
 /**
@@ -24,7 +24,7 @@ gulp.task('build', function (callback) {
 gulp.task('build-clean', function () {
     log('Delete build folder');
     var delbuild = config.build;
-    del(delbuild).then(log('Removed: ' + $.util.colors.white(delbuild)));
+    return del(delbuild).then(log('Removed: ' + $.util.colors.white(delbuild)));
 });
 /**
  * Build JavaScript files
@@ -34,8 +34,30 @@ gulp.task('build-scripts', function () {
     return gulp.src(config.jsfiles)
         .pipe(gulp.dest(config.build + 'app'));
 });
-
-
+/**
+ * Build styles files
+ */
+gulp.task('build-styles', function () {
+    log('Building CSS files...');
+    return gulp.src(config.cssfiles)
+        .pipe(gulp.dest(config.build + 'app'));
+});
+/**
+ * Build vender styles files
+ */
+gulp.task('build-external-styles', function () {
+    log('Building external CSS files...');
+    return gulp.src(config.vendercssfiles)
+        .pipe(gulp.dest(config.build));
+});
+/**
+ * Build Vender JavaScript files
+ */
+gulp.task('build-external-scripts', function () {
+    log('Building external JavaScript files...');
+    return gulp.src(config.venderjsfiles)
+        .pipe(gulp.dest(config.build));
+});
 /**
  * Log a message or series of messages using chalk's blue color.
  * Can pass in a string, object or array.
